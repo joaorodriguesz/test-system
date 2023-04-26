bb1a182baad9096ecb2e2a815a7fd2eff363600c
joao.rodrigues <joaopaulomoreirarodrigues@gmail.com>
2023-04-26 19:58:03 -0300

    install handlebars

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 const express = require("express");
 const navegateRoutes = require('./src/routes/primary.routes');
 const testRoutes = require('./src/routes/test.routes');
+const handlebars = require('express-handlebars');
+const path = require('path');
 
 const app = express();
 
-//Directories 
-app.use(express.static(__dirname + '/public'));
-app.use(express.static(__dirname + '/src'));
+// Teamplate Engine
+    app.engine('handlebars', handlebars.engine({ defaultLayout: 'main' }));
+    app.set('view engine', 'handlebars');
+    app.set('views', path.join(__dirname, '/src', 'views'));
 
-//Routes
-app.use('/', navegateRoutes);
-app.use('/test', testRoutes);
+//  Directories 
+    app.use(express.static(__dirname + '/public'));
+    app.use(express.static(__dirname + '/src'));
+
+//  Routes
+    app.use('/', navegateRoutes);
+    app.use('/test', testRoutes);
 
 app.listen(8081, ()=>{
     console.log("Online: http://localhost:8081");
--- src/routes/primary.routes.js
+++ src/routes/primary.routes.js
@@ -1,15 +1,14 @@
+const { log } = require('console');
 const express = require('express');
 const router = express.Router();
-const path = require('path');
-const viewDirectoriPath = path.join(__dirname, '..', 'views');
 
 router.get("/", (req, res) => {
     res.send("Home")
 });
 
 router.get("/login", (req, res) => {
-    console.log(__dirname);
-    res.sendFile(`${viewDirectoriPath}/login.html`);
+
+    res.render('login', {title: 'Login'});
 });
 
 module.exports = router;
